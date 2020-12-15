aabf41fee78f29605758669cb85995ce1c5765ff
jyp <bpom614@gmail.com>
2020-12-15 17:04:24 +0900

    1215 알리미함수

--- function_test/test_main.py
+++ function_test/test_main.py
@@ -1,26 +1,74 @@
-#  import serial
-#  import firebase_admin
-#  from firebase_admin import credentials
-#  from firebase_admin import firestore
+import serial
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+import schedule
+import pygame
+import time
+import requests
+import urllib.request
+import pandas as pd
 
 #1215  #+++++지정한 이름으로 Firestore에 작성+++++++++
-from weather_alarm import  Tweather
-from weather_alarm import  temperature
+from weather_alarm import Tweather
+from weather_alarm import temperature
 from medicine_alarm import medicine
+ 
+#+++++++++++++++++++++++++++++++++++++++++++
 from plants_gas_flame import gas
 from plants_gas_flame import flame
 from plants_gas_flame import bright
 from plants_gas_flame import soil
 from plants_gas_flame import humid
 from plants_gas_flame import temp
-#from test_function import scan
 
-# Tweather()
-# temperature()
-# medicine()
+
+
+# weather_timeset
+set_time= "16:59"
+
+# drug_timeset
+set_drug = "17:00"
+
+schedule.every().day.at(set_time).do(Tweather)
+schedule.every().day.at(set_time+":06").do(temperature)
+schedule.every().day.at(set_drug).do(medicine) # 매일 해당 시각에 medicine 함수를 실행하겠습니다.
+
+while True:
+    schedule.run_pending()
+
+
+
+#+++++++++++++++++++++This is plants_gas_flame alarm++++++++++++++++
+    
+cred = credentials.Certificate("damsohwa-4d3f4-firebase-adminsdk-okod4-5427e232ce.json")
+firebase_admin.initialize_app(cred)
+db=firestore.client()
+
+
+doc_ref = db.collection(u'Damsohwa').document(u'RaspberryPi')
+
+
+try:
+    doc = doc_ref.get()
+    print(u'Document data : {}'.format(doc.to_dict()))
+
+except :
+    print(u'No such document!')
+
+
+doc.to_dict()
+data_df = pd.DataFrame(doc.to_dict(), index=[0]).astype('float')
+
+
+
+
+data = doc.to_dict()
+
+
 
 if float(data['gas']) <= 14:
-     gas()
+    gas()
 if float(data['flame']) <= 1000:
    flame()
 if float(data['bright']) <= 143:
@@ -30,11 +78,4 @@ if float(data['soil']) <= 1003.0:
 if float(data['humid']) <= 50:
     humid()
 if float(data['temp']) <= 23:
-    temp()
-
-# scan("gas")
-# scan("flame")
-# scan("bright")
-# scan("soil")
-# scan("humid")
-# scan("temp")
+    temp()    
