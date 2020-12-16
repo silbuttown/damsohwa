750ee9cd5d9313912f0eb04db35525209589f9ed
jyp <bpom614@gmail.com>
2020-12-16 12:09:35 +0900

    1216 통합알림 및 시간체크기능 함수화

--- function_test/test_main.py
+++ function_test/test_main.py
@@ -12,21 +12,24 @@ import pandas as pd
 #1215  #+++++지정한 이름으로 Firestore에 작성+++++++++
 from weather_alarm import Tweather , temperature
 from medicine_alarm import medicine
- #+++++++++++++++++++++++++++++++++++++++++++
+#+++++++++++++++++++++++++++++++++++++++++++
 
 from plants_gas_flame import gas, flame, bright, soil, humid, temp
 from test_function import Scan
+
+# +++++++++++++++++++++++++++++++++++++++++++++
+from timecheck_firebase import Check, Check2
+
 
 # Target document
 DOCUMENT = 'plants_gas_flame'
 
 # weather_timeset
-
-set_time= "10:24"
+set_time= "12:06"
 
 # drug_timeset
+set_drug = "12:06:08"
 
-set_drug = "10:25:08"
 
 schedule.every().day.at(set_time).do(Tweather)
 schedule.every().day.at(set_time+":06").do(temperature)
@@ -34,7 +37,9 @@ schedule.every().day.at(set_drug).do(medicine)
 # 매일 해당 시각에 medicine 함수를 실행하겠습니다.
 
 
-# get data from Arduino
+
+
+#get data from Arduino
 line = Scan(DOCUMENT)
 
 gas1,flame1,bright1,soil1,humid1,temp1 = line.split(',')
@@ -42,18 +47,20 @@ gas1,flame1,bright1,soil1,humid1,temp1 = float(gas1),float(flame1),float(bright1
 print("Data from Arduino")
 print("gas : {} flame : {} bright : {} soil : {} humid : {} temp : {}".format(gas1,flame1,bright1,soil1,humid1,temp1))
 
+schedule.every(5).seconds.do(Check,soil1)
+schedule.every(10).seconds.do(Check2,soil1)
 
-if float(gas1) <= 14:
+if gas1 <= 14:
     gas()
-if float(flame1) <= 1000:
+if flame1 <= 1000:
     flame()
-if float(bright1) <= 143:
+if bright1 <= 143:
     bright()
-if float(soil1) <= 1003.0:
+if soil1 <= 1003.0:
     soil()
-if float(humid1) <= 30:
+if humid1 <= 30:
     humid()
-if float(temp1) <= 23:
+if temp1 <= 23:
     temp()
     
     
